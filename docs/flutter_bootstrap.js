83a0901126e4fd27b5368e982ff015bff15c8bd2
kintaikanri37341-star <kintaikanri37341@gmail.com>
2026-05-31 13:50:42 +0000

    enable auto-update

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3213947615"
+    serviceWorkerVersion: "692626296"
   }
 });
